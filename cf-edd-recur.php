1da935c63fc1aa62f57bc7655407947d1bc3efcd
Josh Pollock <Josh@JoshPress.net>
2017-04-04 19:36:25 -0400

    Version: 0.0.4.1

--- cf-edd-recur.php
+++ cf-edd-recur.php
@@ -2,7 +2,7 @@
 /**
  Plugin Name: Caldera Forms EDD Recurring
  Description: Support layer for EDD recurring payments for Caldera Forms EDD
- Version: 0.0.4
+ Version: 0.0.4.1
  */
 //CF58c9fd77084c3
 /**
